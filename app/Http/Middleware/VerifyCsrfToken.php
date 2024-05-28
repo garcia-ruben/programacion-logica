3228696982f92d33cbdf46e1496b88ab2f696ac8
garcia-ruben <rubengarciap98@gmail.com>
2024-05-28 10:03:29 -0600

    1.2.1 CSRF

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 class VerifyCsrfToken extends Middleware
 {
     /**
